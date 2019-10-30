7d44b4a34c86450091ae41fd9053485289d5d7bf
Avinash-Ahilesan <avinash.ahilesan00@gmail.com>
2019-10-30 11:42:46 -0700

    made TrackerRecording Abstract

--- src/main/model/TrackerRecording.java
+++ src/main/model/TrackerRecording.java
@@ -3,7 +3,7 @@ package model;
 import java.io.Serializable;
 import java.util.Calendar;
 
-public class TrackerRecording implements Serializable {
+public abstract class TrackerRecording implements Serializable {
     protected Calendar calendar;
 
     public Calendar getDate() {
